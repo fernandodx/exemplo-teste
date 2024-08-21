1c5ab3b637ab82aecc3e28c3b62bf24a1593a0be
Fernando Dias <fernando.dias@sicoob.com.br>
2024-08-21 01:20:22 -0300

    Config lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,19 @@
 import globals from "globals";
-
+import { ESLint } from "eslint";
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module", // Alterado para 'module' se você estiver usando ES Modules
+      globals: globals.browser,
+    },
+    rules: {
+      semi: ["error", "always"], // Exige ponto-e-vírgula
+      indent: ["error", 2], // Exige indentação de 2 espaços
+      "no-unused-vars": "error", // Erro para variáveis não utilizadas
+      "no-mixed-spaces-and-tabs": "error", // Erro para mistura de espaços e tabs
+      "eol-last": ["error", "always"], // Exige uma linha em branco no final do arquivo
+    },
+  },
 ];
--- soma.js
+++ soma.js
@@ -2,4 +2,8 @@ function soma(a, b) {
   return b + a;
 }
 
+function multiplicacao(a, b) {
+  return a * b;
+}
+
 module.exports = soma;
